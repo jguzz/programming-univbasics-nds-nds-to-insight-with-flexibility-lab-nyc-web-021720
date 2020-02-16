bdcd5c6f279b3312b4e54b70e6fde1cdb41ddeb4
Joseph Guzzardo <joseph.a.guzzardo@gmail.com>
2020-02-16 15:26:16 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -76,16 +76,17 @@ def gross_per_studio(collection)
   #
   # Hash whose keys are the studio names and whose values are the sum
   # total of all the worldwide_grnmoss numbers for every movie in the input Hash
-  totals = []
-  index = 0
-  gross = 0
-  while collection.length > index do
-    studio = collection[index][:studio]
-    gross += collection[index][:worldwide_gross]
-    totals.push({:studio => studio, :gross => gross})
-    index += 1
-  end
-  totals
+  
+  # totals = []
+  # index = 0
+  # gross = 0
+  # while collection.length > index do
+  #   studio = collection[index][:studio]
+  #   gross += collection[index][:worldwide_gross]
+  #   totals.push({:studio => studio, :gross => gross})
+  #   index += 1
+  # end
+  # totals
 end
 
 def movies_with_directors_set(source)
@@ -99,7 +100,7 @@ def movies_with_directors_set(source)
   #
   # Array of Arrays containing all of a director's movies. Each movie will need
   # to have a :director_name key added to it.]
-  
+
   # totals = []
   # index = 0
   # gross = 0
